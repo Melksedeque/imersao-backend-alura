30b35edab1ed32be981f3750ba0c4b76049117b8
Melksedeque Silva <freelancer@melksedeque.com.br>
2024-11-22 21:36:48 -0300

    Controller da função de atualizar post

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,6 @@
 import fs from "fs";
-import {getTodosPosts, postar} from "../models/postsModel.js";
+import {atualizar, getTodosPosts, postar} from "../models/postsModel.js";
+import gerarDescricaoComGemini from "../services/geminiServices.js";
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
@@ -24,6 +25,25 @@ export async function criarPost(req, res) {
     try {
         const postCriado = await postar(novoPost);
         res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro" : "Falha na requisição"})
+    }
+}
+
+export async function atualizarPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.jpg`;
+    try {
+        const imgBuffer = fs.readFileSync(`uploads/${id}.jpg`);
+        const descricao = await gerarDescricaoComGemini(imgBuffer);
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
+        const postAtualizado = await atualizar(id, post);
+        res.status(200).json(postAtualizado);
     } catch(erro) {
         console.error(erro.message);
         res.status(500).json({"Erro" : "Falha na requisição"})
